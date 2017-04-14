db3d0248628b6af43ac70850e41e9f767e0ca4a0
Josip Susic <josip.susic@hotmail.com>
2017-04-14 18:57:51 +0200

    added AddTodo component

---
+++ app/tests/components/AddTodo.test.jsx
@@ -0,0 +1,37 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var AddTodo = require('AddTodo');
+
+describe('AddTodo', () => {
+    it('should exist', () => {
+        expect(AddTodo).toExist();
+    });
+    
+   it('should call onAddTodo if field is not empty', () => {
+       var todoText = 'Check';
+       var spy = expect.createSpy();
+       var todoForm = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy} />);
+       var $el = $(ReactDOM.findDOMNode(todoForm));
+        
+       todoForm.refs.todoText.value = todoText;
+       TestUtils.Simulate.submit($el.find('form')[0]);
+        
+       expect(spy).toHaveBeenCalledWith(todoText);
+    });
+    
+    it('should NOT call onAddTodo if field is empty', () => {
+       var todoText = '';
+       var spy = expect.createSpy();
+       var todoForm = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy} />);
+       var $el = $(ReactDOM.findDOMNode(todoForm));
+        
+       todoForm.refs.todoText.value = todoText;
+       TestUtils.Simulate.submit($el.find('form')[0]);
+        
+       expect(spy).toNotHaveBeenCalled();
+    });
+});
