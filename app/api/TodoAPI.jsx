532b3319e2aada23616b5009e69f9c9cdac289b8
Josip Susic <josip.susic@hotmail.com>
2017-04-18 13:08:00 +0200

    add filters

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -1,4 +1,4 @@
-var $ = require('jQuery');
+var $ = require('jquery');
 
 module.exports = {
     setTodos: function(todos) {
@@ -23,5 +23,34 @@ module.exports = {
         
         return $.isArray(todos) ? todos : [];
         
+    },
+
+    filterTodos: function(todos, showCompleted, searchText) {
+        var filteredTodos = todos;
+
+        // filter by showCompleted
+        filteredTodos = filteredTodos.filter((todo) => {
+            return !todo.completed || showCompleted;
+        }); 
+
+        // filter by searchText
+        filteredTodos = filteredTodos.filter((todo) => {
+            var text = todo.text.toLowerCase();
+            return searchText.length === 0 || text.indexOf(searchText) > -1;
+        });
+
+        // sort with non-completed first
+        filteredTodos.sort((a, b) => {
+            //if a is not completed it should come first
+            if(!a.completed && b.completed) {
+                return -1;
+            } else if (a.completed && !b.completed) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+
+        return filteredTodos;
     }
 };
