f26fc4ea5a27848bc806f26b9e3a848230145e8a
Josip Susic <josip.susic@hotmail.com>
2017-05-03 15:42:47 +0200

    add messaging sender id and set env variable on heroku

--- firebase/index.js
+++ firebase/index.js
@@ -5,7 +5,8 @@ try {
         apiKey: process.env.API_KEY,
         authDomain: process.env.AUTH_DOMAIN,
         databaseURL: process.env.DATABASE_URL,
-        storageBucket: process.env.STORAGE_BUCKET
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID
     };
     
     firebase.initializeApp(config);   
